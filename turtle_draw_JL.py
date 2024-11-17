7946acfd8531bb740d78750609794c95beb20500
Mr. Milker <jonathanrlobash@lewisu.edu>
2024-11-16 18:17:17 -0600

    Update files.

--- turtle_draw_JL.py
+++ turtle_draw_JL.py
@@ -1,20 +1,14 @@
 import turtle
 window = turtle.Screen()
 window.screensize(450, 450)
-"""
-spiral = turtle.Turtle()
-spiral.speed(10)
-
-spiral.pencolor("purple")
-for i in range(40):
-    spiral.forward(i * 10)
-    spiral.right(144)
-
-
-turtle.done()
-"""
 TEXTFILENAME = 'turtle-draw.txt'
 
+filename = input("Enter file name:")
+print("file name is: " + filename)
+TEXTFILENAME=filename
+
+Bob=turtle.Turtle()
+Bob.speed(10)
 turtleDrawTextFile = open(TEXTFILENAME, 'r')
 line = turtleDrawTextFile.readline()
 while line:
@@ -23,6 +17,17 @@ while line:
 
     returnStrings = line.split()
     if( len(returnStrings) == 1):
-        print(returnStrings [0])
+        Bob.penup()
+    elif( len(returnStrings)==3):
+        colors = returnStrings[0]
+        x = int(returnStrings[1])
+        y = int(returnStrings[2])
+
+        Bob.color(colors)
+        Bob.goto(x,y)
+        Bob.pendown()
+
+turtle.done()
+turtleDrawTextFile.close()
 
 print('End')
